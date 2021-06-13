84b3c0c90723725ff4f4b6786d7a6e702a3fdbd6
srivarshinibubbles <79245102+srivarshinibubbles@users.noreply.github.com>
2021-06-13 16:05:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,113 @@
+var canvas = new fabric.Canvas("myCanvas");
+
+block_image_width = 30;
+block_image_height = 30;
+
+player_x = 10;
+player_y = 10;
+var player_object = "";
+
+
+function player_update()
+{
+  fabric.Image.fromURL("player.png", function(Img){
+    player_object = Img;
+    
+    player_object.scaleToWidth(150);
+    player_object.scaleToWidth(140);
+    player_object.set({
+    top:player_y;
+    left:player_x;
+      
+      
+    });
+    canvas.add(player_object);
+    
+  });
+}
+
+function new_image(get_image)
+{
+  fabric.Image.URL(get_image, function(Img){
+    block_image_object = Img;
+    
+    block_image_object.scaleToWidth(block_image_width);
+    block_image_object.scaleToWidth(block_image_height);
+    block_image_object.set({
+      top:player_y;
+      left:player_x;
+    });
+    canvas.add(block_image_object);
+  });
+}
+
+windows.addEventListener("keydown, my_keydown")
+{
+    function my_keydown(e)
+    {
+        keyPressed = e.keyCode;
+        console.log(keyPressed);
+        if(e.shiftKey == true && keyPressed == "77")
+        {
+            console.log("m and shiftKey pressed together");
+            block_image_width = block_image_width - 10;
+            block_image_height = block_image_height - 10;
+            document.getElementById("current_width").innerHTML = block_image_width;
+            document.getElementById("current_height").innerHTML = block_image_height;
+
+        }
+
+        if(keyPressed == "38")
+        {
+          up();
+          console.log("up");
+        }
+      if(keyPressed == "40")
+        {
+          down();
+          console.log("down")
+        }
+      if(keyPressed == "37")
+        {
+          left();
+          console.log("left")
+        }
+      if(keyPressed == "39")
+        {
+          right();
+          console.log("right")
+        }
+
+        
+        if(keyPressed == "70")
+        {
+          new_image("iron_face.png")
+          console.log("f")
+      }
+
+      if(keyPressed == "66")
+  {
+    new_image("spiderman_body(1).png")
+    console.log("b")
+}
+if(keyPressed == "76")
+  {
+    new_image("hulk_legs.png")
+    console.log("l")
+}
+
+if(keyPressed == "82")
+  {
+    new_image("thor_right_hand.png")
+    console.log("r")
+}
+
+if(keyPressed == "72")
+  {
+    new_image("captain_america_left_hand.png")
+    console.log("h")
+}
+
+        
+    }
+}
